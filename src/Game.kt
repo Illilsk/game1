1938971d9c78b602387323d357fce6f240f51b09
Illilsk <ilyasurmancheev@gmail.com>
2022-03-16 22:51:27 +0700

    Карма

--- src/Game.kt
+++ src/Game.kt
@@ -1,14 +1,17 @@
+import kotlin.math.pow
+
 fun main(args: Array<String>) {
     val userName = "Madrigal"
-    var healthPoints = 100
+    var healthPoints = 10
     val isBlessed = true
     val isImmortal = false
     var auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "Green aura" else "None aura"
     val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    val userKarma = checkKarma(healthPoints)
     println("(Aura: $auraColor)" + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$userName $healthStatus")
-    println("xyi")
+    println("$userName $userKarma")
 }
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
@@ -21,4 +24,14 @@ private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = 
     }
     in 15..49 -> " looks pretty hurt.."
     else -> " is in awful condition:("
+}
+
+private fun checkKarma(healthPoints: Int): String {
+    return when ((Math.random().pow((110 - healthPoints) / 100.0) * 20).toInt()) {
+        in 0..5 -> "red"
+        in 6..10 -> "orange"
+        in 11..15 -> "purple"
+        in 16..20 -> "green"
+        else -> "nothing"
+    }
 }
