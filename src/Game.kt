d19a40549a34dd61010caed4f2ee606c5cef7a2b
Illilsk <ilyasurmancheev@gmail.com>
2022-03-16 22:03:45 +0700

    ASDFA

---
+++ src/Game.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>) {
+    val userName = "Madrigal"
+    var healthPoints = 100
+    val isBlessed = true
+    val isImmortal = false
+    var auraVisible = isBlessed && healthPoints > 50 || isImmortal
+    val auraColor = if (auraVisible) "Green aura" else "None aura"
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+    println("(Aura: $auraColor)" + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$userName $healthStatus")
+}
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String
+     = when (healthPoints) {
+        100 -> " is in full condition!"
+        in 90..99 -> " has a few scratches!"
+        in 50..89 -> if (isBlessed) {
+            " has some minor wounds but is healing quite quickly!"
+        } else {
+            " has some minor wounds."
+        }
+        in 15..49 -> " looks pretty hurt.."
+        else -> " is in awful condition:("
+    }
---
+++ src/Hello.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println("Hello, world!")
+    println(playerName)
+    println(experiencePoints)
+    val hasSteed = false
+    val tavernName = "Rog of Unicorn"
+    val innkeeperName = "Ignat"
+    var moneyCount: Double = 50.0
+    val menuList: List<String> = listOf("honey","beer","vine")
+}
