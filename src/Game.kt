533f68f82ec3c78f7f0ff05fa8093c2fe8b206e7
Illilsk <ilyasurmancheev@gmail.com>
2022-03-16 22:07:10 +0700

    xrt

--- src/Game.kt
+++ src/Game.kt
@@ -9,15 +9,15 @@ fun main(args: Array<String>) {
     println("(Aura: $auraColor)" + "(Blessed: ${if (isBlessed) "YES" else "NO"})")
     println("$userName $healthStatus")
 }
-private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String
-     = when (healthPoints) {
-        100 -> " is in full condition!"
-        in 90..99 -> " has a few scratches!"
-        in 50..89 -> if (isBlessed) {
-            " has some minor wounds but is healing quite quickly!"
-        } else {
-            " has some minor wounds."
-        }
-        in 15..49 -> " looks pretty hurt.."
-        else -> " is in awful condition:("
+
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
+    100 -> " is in full condition!"
+    in 90..99 -> " has a few scratches!"
+    in 50..89 -> if (isBlessed) {
+        " has some minor wounds but is healing quite quickly!"
+    } else {
+        " has some minor wounds."
     }
+    in 15..49 -> " looks pretty hurt.."
+    else -> " is in awful condition:("
+}
