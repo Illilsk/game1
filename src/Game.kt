b8395e63cef3ac23a23e47567cda7a0a4db87ff4
Illilsk <ilyasurmancheev@gmail.com>
2022-03-16 23:07:42 +0700

    хп, аура

--- src/Game.kt
+++ src/Game.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
     var auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "Green aura" else "None aura"
     println("(HP: $healthPoints)(Aura: $auraColor) -> $userName ${formatHealthStatus(healthPoints, isBlessed)}")
+    formatHealthStatus(5.5.toInt(), true)
 }
 
 private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
